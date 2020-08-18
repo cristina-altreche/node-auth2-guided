ed2ce369189ba5d69a2acaa4e41df5af3c022cd3
Cristina Altreche <c.altreche@gmail.com>
2020-08-18 14:49:33 -0400

    initial

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,9 +1,10 @@
 const bcryptjs = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
 const router = require("express").Router();
 
 const Users = require("../users/users-model.js");
 const { isValid } = require("../users/users-service.js");
+const constants = require("../config/constants");
 
 router.post("/register", (req, res) => {
   const credentials = req.body;
@@ -18,15 +19,16 @@ router.post("/register", (req, res) => {
 
     // save the user to the database
     Users.add(credentials)
-      .then(user => {
+      .then((user) => {
         res.status(201).json({ data: user });
       })
-      .catch(error => {
+      .catch((error) => {
         res.status(500).json({ message: error.message });
       });
   } else {
     res.status(400).json({
-      message: "please provide username and password and the password shoud be alphanumeric",
+      message:
+        "please provide username and password and the password shoud be alphanumeric",
     });
   }
 });
@@ -39,19 +41,36 @@ router.post("/login", (req, res) => {
       .then(([user]) => {
         // compare the password the hash stored in the database
         if (user && bcryptjs.compareSync(password, user.password)) {
-          res.status(200).json({ message: "Welcome to our API" });
+          const token = signToken(user);
+          res.status(200).json({ message: "Welcome to our API", token });
         } else {
           res.status(401).json({ message: "Invalid credentials" });
         }
       })
-      .catch(error => {
+      .catch((error) => {
         res.status(500).json({ message: error.message });
       });
   } else {
     res.status(400).json({
-      message: "please provide username and password and the password shoud be alphanumeric",
+      message:
+        "please provide username and password and the password shoud be alphanumeric",
     });
   }
 });
 
+//use jwt library
+function signToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    role: user.role,
+  };
+  // const secret = process.env.JWT_SECRET || "is it secret, is it safe?";
+  const secret = constants.jwtSecret;
+  const options = {
+    expiresIn: "1d",
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 module.exports = router;
